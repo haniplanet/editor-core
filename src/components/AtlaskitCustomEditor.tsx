ee2b6061f5fe527f8886e08412d444785e2fab69
jungpaeng <meis1541@naver.com>
2019-05-07 10:29:02 +0900

    :hammer: 테스트용 에디터 노출

--- src/components/AtlaskitCustomEditor.tsx
+++ src/components/AtlaskitCustomEditor.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { Editor } from '@atlaskit/editor-core';
 
 class AtlaskitCustomEditor extends React.Component {
   render() {
     return (
       <div>
         <p>Atlaskit Custom Editor</p>
+        <Editor />
       </div>
     );
   }
