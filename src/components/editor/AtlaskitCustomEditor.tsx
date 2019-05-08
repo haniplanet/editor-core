0c0e26c3ec760129d6bf09530da8c1d37f483f65
jungpaeng <meis1541@naver.com>
2019-05-08 10:28:41 +0900

    :sparkles: Custom Extension 에디터에 추가

--- src/components/editor/AtlaskitCustomEditor.tsx
+++ src/components/editor/AtlaskitCustomEditor.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Editor } from '@atlaskit/editor-core';
+import { ExtensionHandlers } from '@atlaskit/editor-common';
 import ToolsDrawer from './ToolsDrawer';
 import extensionHandlers from './helpers/extensionHandlers';
 import selectMockMenu from './helpers/selectMockMenu';
@@ -7,16 +8,23 @@ import { ICustomButton } from '../../types/editor';
 
 interface IProps {
   customButton?: ICustomButton[];
+  customExtensions?: ExtensionHandlers;
 }
 
-const AtlaskitCustomEditor: React.FC<IProps> = ({ customButton }) => (
+const AtlaskitCustomEditor: React.FC<IProps> = ({
+  customButton,
+  customExtensions,
+}) => (
   <ToolsDrawer
     customButton={customButton}
     isImageUpload={true}
     renderEditor={({ legacyImageUploadProvider, fileUploadMenuItem }) => (
       <Editor
         appearance="comment"
-        extensionHandlers={extensionHandlers}
+        extensionHandlers={{
+          ...extensionHandlers,
+          ...customExtensions,
+        }}
         insertMenuItems={[...selectMockMenu(['movie']), fileUploadMenuItem]}
         legacyImageUploadProvider={legacyImageUploadProvider}
         allowExtension={true}
