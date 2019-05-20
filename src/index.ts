ed2d680b14f75e2fa8b78cd504973eb79a2324ff
jungpaeng <meis1541@naver.com>
2019-05-20 14:02:14 +0900

    HDK-97 :books: CustomSVG export 및 문서 정리

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import AtlaskitCustomEditor from './components/editor/AtlaskitCustomEditor';
 import AtlaskitCustomRenderer from './components/renderer/AtlaskitCustomRenderer';
+import CustomSVG from './components/common/CustomSVG';
 import { createEditorMenuItem } from './lib/editor';
 
-export { AtlaskitCustomEditor, AtlaskitCustomRenderer, createEditorMenuItem };
+export {
+  AtlaskitCustomEditor,
+  AtlaskitCustomRenderer,
+  CustomSVG,
+  createEditorMenuItem,
+};
