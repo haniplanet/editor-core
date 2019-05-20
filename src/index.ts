413f0043e1c3e7607e1d1bccc2ba610ee9fb4b95
jungpaeng <meis1541@naver.com>
2019-05-20 13:10:25 +0900

    HDK-97 :sparkles: 에디터 메뉴 작성 함수 export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import AtlaskitCustomEditor from './components/editor/AtlaskitCustomEditor';
 import AtlaskitCustomRenderer from './components/renderer/AtlaskitCustomRenderer';
+import { createEditorMenuItem } from './lib/editor';
 
-export { AtlaskitCustomEditor, AtlaskitCustomRenderer };
+export { AtlaskitCustomEditor, AtlaskitCustomRenderer, createEditorMenuItem };
