ed195ea7424bab731efe7c713ece11db87329c7e
jungpaeng <meis1541@naver.com>
2019-06-05 10:50:03 +0900

    :microscope: media Renderer 테스트 haniplanet/issues#33

--- src/components/renderer/AtlaskitCustomRenderer.tsx
+++ src/components/renderer/AtlaskitCustomRenderer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { ReactRenderer } from '@atlaskit/renderer';
-import { ExtensionHandlers } from '@atlaskit/editor-common';
+import { ExtensionHandlers, ProviderFactory } from '@atlaskit/editor-common';
+import { storyMediaProviderFactory } from '@atlaskit/editor-test-helpers';
 import extensionHandlers from '../editor/helpers/extensionHandlers';
 
 interface IProps {
@@ -8,12 +9,19 @@ interface IProps {
   customExtensions?: ExtensionHandlers;
 }
 
+const mediaProvider = storyMediaProviderFactory();
+
+const providerFactory = ProviderFactory.create({
+  mediaProvider,
+});
+
 const AtlaskitCustomRenderer: React.FC<IProps> = ({
   editorValue,
   customExtensions,
 }) => (
   <ReactRenderer
     document={editorValue}
+    dataProviders={providerFactory}
     extensionHandlers={{
       ...extensionHandlers,
       ...customExtensions,
