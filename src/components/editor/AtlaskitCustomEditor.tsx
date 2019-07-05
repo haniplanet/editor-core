f5c6a204b665e75a43f610d6cd6a34442ce40fc3
jungpaeng <meis1541@naver.com>
2019-07-05 16:51:20 +0900

    :sparkles: console.log 임시 추가

--- src/components/editor/AtlaskitCustomEditor.tsx
+++ src/components/editor/AtlaskitCustomEditor.tsx
@@ -38,38 +38,48 @@ const AtlaskitCustomEditor: React.FC<IProps> = ({
     allowTextAlignment: true,
     allowExtension: true,
   },
-}) => (
-  <ToolsDrawer
-    customButton={customButton}
-    customActionButton={customActionButton}
-    isImageUpload={basicExtension.isMediaExtension}
-    renderEditor={({ fileUploadMenuItem, imageUploadMenuItem }) => {
-      const { isMediaExtension, isMovieExtension } = basicExtension;
-      let basicInsertMenu: InsertMenuCustomItem[] = [];
+}) => {
+  console.log('basicExtension', basicExtension);
+  console.log('basicMockMenu', basicMockMenu);
+  console.log('customButton', customButton);
+  console.log('customActionButton', customActionButton);
+  console.log('customExtensions', customExtensions);
+  console.log('defaultValue', defaultValue);
+  console.log('editorProps', editorProps);
 
-      basicInsertMenu = isMediaExtension
-        ? [...basicInsertMenu, imageUploadMenuItem]
-        : basicInsertMenu;
-      basicInsertMenu = isMovieExtension
-        ? [...basicInsertMenu, fileUploadMenuItem]
-        : basicInsertMenu;
+  return (
+    <ToolsDrawer
+      customButton={customButton}
+      customActionButton={customActionButton}
+      isImageUpload={basicExtension.isMediaExtension}
+      renderEditor={({ fileUploadMenuItem, imageUploadMenuItem }) => {
+        const { isMediaExtension, isMovieExtension } = basicExtension;
+        let basicInsertMenu: InsertMenuCustomItem[] = [];
 
-      return (
-        <Editor
-          defaultValue={defaultValue}
-          extensionHandlers={{
-            ...extensionHandlers(basicExtension),
-            ...customExtensions,
-          }}
-          insertMenuItems={[
-            ...basicInsertMenu,
-            ...selectMockMenu(basicMockMenu),
-          ]}
-          {...editorProps}
-        />
-      );
-    }}
-  />
-);
+        basicInsertMenu = isMediaExtension
+          ? [...basicInsertMenu, imageUploadMenuItem]
+          : basicInsertMenu;
+        basicInsertMenu = isMovieExtension
+          ? [...basicInsertMenu, fileUploadMenuItem]
+          : basicInsertMenu;
+
+        return (
+          <Editor
+            defaultValue={defaultValue}
+            extensionHandlers={{
+              ...extensionHandlers(basicExtension),
+              ...customExtensions,
+            }}
+            insertMenuItems={[
+              ...basicInsertMenu,
+              ...selectMockMenu(basicMockMenu),
+            ]}
+            {...editorProps}
+          />
+        );
+      }}
+    />
+  );
+};
 
 export default AtlaskitCustomEditor;
