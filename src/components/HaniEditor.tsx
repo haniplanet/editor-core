f773fb0dfa2bf1efb4bb77af071d5a47d29b42cd
Im Yongbeen <meis1541@naver.com>
2019-07-10 17:40:18 +0900

    에디터에 영상 확장 기능 적용

--- src/components/HaniEditor.tsx
+++ src/components/HaniEditor.tsx
@@ -1,19 +1,32 @@
 import * as React from 'react';
+import { ExtensionHandlers } from '@atlaskit/editor-common';
 import { Editor, EditorProps, EditorActions } from '@atlaskit/editor-core';
 import MenuDrawer from './menu/MenuDrawer';
+import extensionHandlers, {
+  IBasicExtension,
+} from './extension/extensionsHandler';
+import extensionsMenu, { TExtensionsMenu } from './extension/extensionsMenu';
 import { ICustomButtom } from '../../types/editor';
 
 interface IHaniEditorProps {
+  basicExtension?: IBasicExtension;
+  basicExtensionMenu?: TExtensionsMenu;
   defaultValue?: Pick<EditorProps, 'defaultValue'>;
   customButton: ICustomButtom[];
   customActionButton?: (actions: EditorActions) => React.ReactElement[];
+  customExtensions?: ExtensionHandlers;
   editorProps?: EditorProps;
 }
 
 const AtlaskitCustomEditor: React.FC<IHaniEditorProps> = ({
+  basicExtension = {
+    isMovieExtension: true,
+  },
+  basicExtensionMenu = ['movie'],
   defaultValue,
   customButton,
   customActionButton,
+  customExtensions,
   editorProps = {},
 }) => (
   <MenuDrawer
@@ -35,10 +48,15 @@ const AtlaskitCustomEditor: React.FC<IHaniEditorProps> = ({
           allowTextAlignment={true}
           allowExtension={true}
           defaultValue={defaultValue}
+          extensionHandlers={{
+            ...extensionHandlers(basicExtension),
+            ...customExtensions,
+          }}
           insertMenuItems={[
             ...customButton,
             fileUploadMenuItem,
             imageUploadMenuItem,
+            ...extensionsMenu(basicExtensionMenu),
           ]}
           {...editorProps}
         />
