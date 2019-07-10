0a85669b0e02600cefb036bf6c290e68e2aa0cd3
Im Yongbeen <meis1541@naver.com>
2019-07-10 17:39:56 +0900

    영상 확장 기능 작성

---
+++ src/components/extension/movie/extension.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+interface IMovie {
+  id: string;
+  src: string;
+}
+
+const extension = (parameters: any) => {
+  const { type, key } = parameters;
+  const movie: IMovie = {} as IMovie;
+
+  switch (type) {
+    case 'youtube':
+      movie.id = key;
+      movie.src = '//www.youtube.com/embed/';
+      break;
+    case 'vimeo':
+      movie.id = key;
+      movie.src = '//player.vimeo.com/video/';
+      break;
+    default:
+      return null;
+  }
+
+  return (
+    <div>
+      <iframe
+        title={`movie-${movie.id}`}
+        width="560"
+        height="315"
+        src={movie.src + movie.id}
+      />
+    </div>
+  );
+};
+
+export default extension;
