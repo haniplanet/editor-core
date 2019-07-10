3faf488beba176439e5dc7e565001814b7ddb03c
Im Yongbeen <meis1541@naver.com>
2019-07-10 12:45:24 +0900

    editor 반환 빌드

--- build/index.js
+++ build/index.js
@@ -8,8 +8,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __importStar(require("react"));
+var editor_core_1 = require("@atlaskit/editor-core");
 var MasterTemp = React.memo(function () {
-    return (React.createElement("div", null,
-        React.createElement("p", null, "MasterTemp")));
+    return (React.createElement("div", null, editor_core_1.Editor));
 });
 exports.default = MasterTemp;
