4bd02574a6609837790fe721830f6fae1ac10566
Im Yongbeen <meis1541@naver.com>
2019-07-10 16:08:47 +0900

    CustomSVG 컴포넌트 작성
    - svg를 작성할 때 중복되는 부분이 많아 이를 별도의 컴포넌트로 분리

---
+++ src/components/common/CustomSVG.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+interface ICustomSVGProps {
+  width: string;
+  height: string;
+  d: string;
+}
+
+const CustomSVG = React.memo<ICustomSVGProps>(({ width, height, d }) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox={`0 0 ${width} ${height}`}
+    focusable="false"
+    role="presentation"
+  >
+    <path d={d} fill="currentColor" fillRule="evenodd" />
+  </svg>
+));
+
+export default CustomSVG;
