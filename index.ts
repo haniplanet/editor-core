1cc7a0b7f854c0a448c1761ed56c6dcb9800bbb8
jungpaeng <meis1541@naver.com>
2019-11-28 14:46:20 +0900

    :hammer: index.ts 파일 최상위 폴더로 이동

---
+++ index.ts
@@ -0,0 +1,5 @@
+import EditorCore from './src/components/EditorCore';
+import {editorTransformer} from './src/lib';
+
+export {editorTransformer};
+export default EditorCore;
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import EditorCore from './components/EditorCore';
-import {editorTransformer} from './lib';
-
-export {editorTransformer};
-export default EditorCore;
