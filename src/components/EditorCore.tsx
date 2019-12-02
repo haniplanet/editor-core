f138318446fed3fbfd2493a051532b6c05766adb
jungpaeng <meis1541@naver.com>
2019-12-02 14:31:59 +0900

    :hammer: EditorCore 컴포넌트 기능 개선

--- src/components/EditorCore.tsx
+++ src/components/EditorCore.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import isEqual from 'lodash.isEqual';
 import {ExtensionHandlers} from '@atlaskit/editor-common';
 import {Editor, EditorProps, EditorActions} from '@atlaskit/editor-core';
 import MenuDrawer, {IUploadHandler} from './menu/MenuDrawer';
 import extensionHandlers, {IBasicExtension} from './extension/extensionsHandler';
 import extensionsMenu, {TExtensionsMenu} from './extension/extensionsMenu';
+import {editorTransformer} from '../lib/editor';
 import {ICustomButtom} from '../../types/editor';
 
 export interface IEditorCoreProps {
@@ -15,60 +17,85 @@ export interface IEditorCoreProps {
   customExtensions?: ExtensionHandlers;
   editorProps?: EditorProps;
   uploadHandler?: IUploadHandler;
+  editorOnChange: (editorViewToJson: object) => void;
 }
 
-const EditorCore: React.FC<IEditorCoreProps> = ({
-  basicExtension = {
-    isMovieExtension: true,
-    isMediaExtension: true,
-  },
-  basicExtensionMenu = ['movie'],
-  defaultValue,
-  customButton,
-  customActionButton,
-  customExtensions,
-  editorProps = {},
-  uploadHandler,
-}: IEditorCoreProps) => {
-  const {isMediaExtension} = basicExtension;
+class EditorCore extends React.PureComponent<IEditorCoreProps> {
+  componentDidMount() {
+    const {defaultValue, editorOnChange} = this.props;
 
-  return (
-    <div className="hani-editor">
-      <MenuDrawer
-        customButton={customButton}
-        customActionButton={customActionButton}
-        isImageUpload={isMediaExtension ? isMediaExtension : false}
-        uploadHandler={uploadHandler}
-        renderEditor={({customButton, fileUploadMenuItem, imageUploadMenuItem}) => {
-          const imageUploadButton = isMediaExtension ? [imageUploadMenuItem] : [];
+    if (!!editorOnChange && !!defaultValue) {
+      editorOnChange(defaultValue);
+    }
+  }
 
-          return (
-            <Editor
-              appearance="comment"
-              allowLists={true}
-              allowTables={true}
-              allowTextColor={true}
-              allowTextAlignment={true}
-              allowExtension={true}
-              allowCodeBlocks={true}
-              defaultValue={defaultValue}
-              extensionHandlers={{
-                ...extensionHandlers(basicExtension),
-                ...customExtensions,
-              }}
-              insertMenuItems={[
-                ...customButton,
-                fileUploadMenuItem,
-                ...imageUploadButton,
-                ...extensionsMenu(basicExtensionMenu),
-              ]}
-              {...editorProps}
-            />
-          );
-        }}
-      />
-    </div>
-  );
-};
+  componentDidUpdate(prevProps) {
+    const {defaultValue: prevDefaultValue} = prevProps;
+    const {defaultValue, editorOnChange} = this.props;
+
+    if (!!editorOnChange && !isEqual(prevDefaultValue, defaultValue)) {
+      editorOnChange(defaultValue);
+    }
+  }
+
+  render() {
+    const {
+      basicExtension = {
+        isMovieExtension: true,
+        isMediaExtension: true,
+      },
+      basicExtensionMenu = ['movie'],
+      defaultValue,
+      customButton,
+      customActionButton,
+      customExtensions,
+      editorProps = {},
+      uploadHandler,
+      editorOnChange,
+    } = this.props;
+    const {isMediaExtension} = basicExtension;
+
+    return (
+      <div className="hani-editor">
+        <MenuDrawer
+          customButton={customButton}
+          customActionButton={customActionButton}
+          isImageUpload={isMediaExtension ? isMediaExtension : false}
+          uploadHandler={uploadHandler}
+          renderEditor={({customButton, fileUploadMenuItem, imageUploadMenuItem}) => {
+            const imageUploadButton = isMediaExtension ? [imageUploadMenuItem] : [];
+
+            return (
+              <Editor
+                appearance="comment"
+                allowLists={true}
+                allowTables={true}
+                allowTextColor={true}
+                allowTextAlignment={true}
+                allowExtension={true}
+                allowCodeBlocks={true}
+                defaultValue={defaultValue}
+                extensionHandlers={{
+                  ...extensionHandlers(basicExtension),
+                  ...customExtensions,
+                }}
+                insertMenuItems={[
+                  ...customButton,
+                  fileUploadMenuItem,
+                  ...imageUploadButton,
+                  ...extensionsMenu(basicExtensionMenu as TExtensionsMenu),
+                ]}
+                {...editorProps}
+                onChange={editorView => {
+                  editorOnChange(editorTransformer.encode(editorView.state.doc));
+                }}
+              />
+            );
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default EditorCore;
